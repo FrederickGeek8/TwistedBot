dc68a83c108a15478f2c9b42764a9dab24d29457
Fred Morlock <FrederickGeek8@gmail.com>
2012-12-30 14:06:55 -0500

    Bot name generator

--- twistedbot/config.py
+++ twistedbot/config.py
@@ -1,11 +1,16 @@
 import math
+import random
+import string
 
 DEBUG = False
 USE_ENCRYPTION = False
 
-COMMANDER = "lukleh"
+COMMANDER = "FrederickGeek8"
 
-USERNAME = "twistedbot"
+def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for x in range(size))
+
+USERNAME = id_generator()
 SERVER_HOST = "127.0.0.1"
 SERVER_PORT = 25565
 
